fb4558bd6829fa0890ea147a1ed3e18590ef1119
Luke Gessler <ldg3fa@virginia.edu>
2019-08-13 11:23:34 -0400

    Add zero state

--- imports/ui/SentenceList.js
+++ imports/ui/SentenceList.js
@@ -5,20 +5,33 @@ import { withTracker } from 'meteor/react-meteor-data';
 
 import { makeStyles } from '@material-ui/styles';
 import Paper from '@material-ui/core/Paper';
+import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Sentence from "./Sentence";
 
-const useStyles = makeStyles((theme) => {
-
-});
+const useStyles = makeStyles((theme) => ({
+  zeroState: {
+    marginTop: theme.spacing(5),
+    textAlign: "center"
+  }
+}));
 
 export default function SentenceList(props) {
   const styles = useStyles();
+  const zeroState = (
+    <Typography variant="subtitle1" className={styles.zeroState}>
+      No sentences added yet. Use the button to add one.
+    </Typography>
+  );
+
   return (
     <Container maxWidth="md">
-      {props.sentences.map((s) => {
-        return <Sentence sentence={s} key={s._id} />
-      })}
+      {props.sentences.length === 0 ?
+        zeroState
+        :
+        props.sentences.map((s) => {
+          return <Sentence sentence={s} key={s._id} />
+        })}
     </Container>
   )
 }
