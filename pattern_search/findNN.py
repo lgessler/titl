ae32364c3c6e5056754c9ce7e83f63be05751c49
Luke Gessler <ldg3fa@virginia.edu>
2019-08-15 14:45:54 -0400

    perform similary search with KDTree

--- pattern_search/findNN.py
+++ pattern_search/findNN.py
@@ -4,6 +4,7 @@ import re
 import codecs
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import KDTree
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
@@ -13,27 +14,27 @@ all_vectors = None
 all_lines = None
 
 def initialize():
-    global all_lines, all_vectors
+    global all_lines, all_vectors, tree
+    print("Initializing lines and vectors...", end=" ")
 
     with codecs.open(args.all_sents, "r", encoding='utf-8') as fin:
         all_lines = [line.strip().lower() for line in fin.readlines()]
 
     with codecs.open(args.all_sents_vectors,"r", encoding='utf-8') as fin:
-        all_lines_vectors = fin.readlines()
-        all_vectors = []
-        for vec in all_lines_vectors:
-            v = np.asarray(vec.strip().split()).astype(np.float32)
-            all_vectors.append(np.transpose(v.reshape((args.dim,1))))
+        all_vectors = np.loadtxt(fin)
 
-    print("Total lines :{0} Input Lines: {1}".format(len(all_lines), len(given_lines)))
     assert len(all_lines) == len(all_vectors)
+    print("Loaded all lines and vectors.")
+    print("Total lines: {0} Input Lines: {1}".format(len(all_lines), len(given_lines)))
 
-    print("Loaded all vectors.")
-
+    
+    print("Building KDTree... ", end="")
+    tree = KDTree(all_vectors) #metric=cosine_similarity)
+    print(" built.")
 
 @app.route('/', methods=["POST"])
 def compute():
-    global given_lines, all_lines, all_vectors
+    global given_lines, all_lines, all_vectors, tree
 
     if args.input_sent:
         with codecs.open(args.input_sent, "r", encoding='utf-8') as fin:
@@ -44,6 +45,7 @@ def compute():
     else:
         raise Exception("no input")
 
+
     #with codecs.open(args.input_sent_vec,"r", encoding='utf-8') as fin:
     #    given_lines_vectors = fin.readlines()
     #    vectors = []
@@ -52,29 +54,17 @@ def compute():
     #        vectors.append(np.transpose(v.reshape((args.dim,1))))
 
     indexes = [all_lines.index(line.strip().lower()) for line in given_lines]
-    vectors = [all_vectors[i] for i in indexes]
+    vectors = np.array([all_vectors[i] for i in indexes])
     assert len(given_lines) ==  len(vectors)
 
     cosine_similarities = {}
-    for index, vec in enumerate(all_vectors):
-        sent  = all_lines[index].strip()
-        max_cosine = -10.0
-        for input_index, input_vec in enumerate(vectors):
-            input_sent = given_lines[input_index].strip()
-            if input_sent == sent:
-                continue
-
-            cosine_dist = cosine_similarity(vec, input_vec)[0][0]
-            if cosine_dist > max_cosine:
-                max_cosine = cosine_dist
-
-        cosine_similarities[index] = float(max_cosine)
-
-    print("Computed all cosine.")
-    sorted_cosine_simi = sorted(cosine_similarities.items(), key=lambda kv:kv[1], reverse=True)[:args.k]
+    distances, indexes = tree.query(np.array([np.mean(vectors, axis=0)]), k=args.k, sort_results=True)
+    
+    sorted_cosine_simi = list(zip(indexes[0], distances[0]))
 
     sent_dict = {}
     for (sent_index, cosine_value) in sorted_cosine_simi:
+        print(sent_index, cosine_value)
         sent = all_lines[sent_index]
         sent_dict[sent] = cosine_value
 
---
+++ pattern_search/test.sh
@@ -0,0 +1,1 @@
+curl -d "@test.json" -H "Content-Type: application/json" -X POST localhost:5000
