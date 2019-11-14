da335568ae3e3b9515ce07aa2c42ce7ccc612969
Luke Gessler <lukegessler@gmail.com>
2019-11-14 13:35:33 -0500

    add some more constructions

--- embeddings/eval_binary_predict.py
+++ embeddings/eval_binary_predict.py
@@ -3,6 +3,7 @@ import numpy as np
 import conllu
 from collections import defaultdict
 from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
 from tqdm import tqdm
 import sklearn
 
@@ -33,9 +34,42 @@ def classify_by_imperative(sentences):
     return ys
 
 
+def classify_by_indicative(sentences):
+    def classify_sentence(sentence):
+        for token in sentence:
+            if token['feats'] is not None:
+                feats = token['feats']
+                if 'Mood' in feats and feats['Mood'] == "Ind":
+                    return True
+        return False
+
+    ys = []
+    for sentence in sentences:
+        ys.append(classify_sentence(sentence))
+    return ys
+
+
+def classify_by_particle_verb(sentences):
+    def classify_sentence(sentence):
+        for token in sentence:
+            if token['deprel'] == 'compound:prt':
+                return True
+        return False
+
+    ys = []
+    for sentence in sentences:
+        ys.append(classify_sentence(sentence))
+    return ys
+
+
 def classify(sentences, strategy):
-    if strategy == "imperative":
-        return classify_by_imperative(sentences)
+    strategies = {
+        "imperative": classify_by_imperative,
+        "indicative": classify_by_indicative,
+        "particle_verb": classify_by_particle_verb
+    }
+    if strategy in strategies:
+        return np.array(strategies[strategy](sentences))
     else:
         raise Exception(f"Unknown strategy: {strategy}")
 
@@ -47,33 +81,39 @@ def vectorize(sentences, word_vecs):
         words = [word_vecs[token['form']] if token['form'] in word_vecs else unk_vector for token in sentence]
         avg_word_vec = np.mean(words, axis=0)
         sentence_vectors.append(avg_word_vec)
-    return sentence_vectors
+    return np.array(sentence_vectors)
 
 # https://github.com/PrashantRanjan09/Improved-Word-Embeddings
 
 def eval(args):
-    print("loading sentences...", end=" ")
+    print("loading sentences...", end=" ", flush=True)
     sentences = conllu.parse(open(args.conllu_file, 'r').read())
     print(f"loaded {len(sentences)} sentences from {args.conllu_file}.", flush=True)
 
-    print("classifying sentences...", end=" ")
+    print("classifying sentences...", end=" ", flush=True)
     sentence_classes = classify(sentences, args.classify_by)
     print("done.", flush=True)
 
-    print("reading vectors...", end=" ")
+    print("reading vectors...", end=" ", flush=True)
     word_vecs = read_vecs(args.vecs)
     print("done.", flush=True)
 
-    print("vectorizing sentences...", end=" ")
+    print("vectorizing sentences...", end=" ", flush=True)
     sentence_vectors = vectorize(sentences, word_vecs)
     print("done.", flush=True)
 
-    X, X_test, y, y_test = sklearn.model_selection.train_test_split(sentence_vectors, sentence_classes, test_size=0.2)
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(sentence_vectors, sentence_classes, test_size=0.2)
 
-    m = LogisticRegression()
-    m.fit(X, y)
+    #m = LogisticRegression(solver='lbfgs')
+    m = XGBClassifier()
+    m.fit(X_train, y_train)
     y_pred = m.predict(X_test)
-    print("Accuracy: ", sklearn.metrics.accuracy_score(y_test, y_pred))
+    print(f"Proportion:       {sum(sentence_classes)}/{len(sentence_classes)} ({sum(sentence_classes)/len(sentence_classes)})")
+    print(f"Train proportion: {sum(y_train)}/{len(y_train)} ({1 - sum(y_train)/len(y_train)} lack the construction)")
+    print(f"Test proportion:  {sum(y_test)}/{len(y_test)} ({1 - sum(y_test)/len(y_test)} lack the construction)")
+    print("Accuracy:   ", sklearn.metrics.accuracy_score(y_test, y_pred))
+    print("Recall:     ", sklearn.metrics.recall_score(y_test, y_pred))
+    print("F1:         ", sklearn.metrics.f1_score(y_test, y_pred))
 
 
 if __name__ == "__main__":
@@ -87,7 +127,7 @@ if __name__ == "__main__":
         help="a .conllu file to test on"
     )
     parser.add_argument(
-        "--classify_by",
+        "--classify-by",
         help="a strategy for how to decide the binary class of a sentence",
         default="imperative"
     )
